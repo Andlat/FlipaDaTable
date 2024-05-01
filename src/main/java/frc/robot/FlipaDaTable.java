ca14119d9110bbe791bbde21154fd76bd7ee1bb1
Nikola Zelovic <nikola1998@hotmail.fr>
2024-05-01 14:34:07 -0400

    Base project

---
+++ src/main/java/frc/robot/FlipaDaTable.java
@@ -0,0 +1,42 @@
+package frc.robot;
+
+public class FlipaDaTable {
+    private static FlipaDaTable m_instance = null;
+    public static FlipaDaTable getInstance(){
+        if(m_instance == null){
+            m_instance = new FlipaDaTable();
+        }
+        return m_instance;
+    }
+    private FlipaDaTable(){
+    }
+
+    private enum State{
+        PREPARING,
+        FLIPPING,
+    }
+    private State m_state = State.PREPARING;
+    public void nextState(){
+        switch (this.m_state) {
+            case PREPARING:
+                m_state = State.FLIPPING;
+                break;
+            case FLIPPING:
+                m_state = State.PREPARING;
+                break;
+        }
+    }
+
+    public static void reset(){
+        getInstance().m_state = State.PREPARING; 
+    }
+
+    public static String flip(){
+        // https://emoticoncentral.com/category/table-flipping
+
+        if(getInstance().m_state == State.PREPARING)
+            return "┬─┬ノ( º _ ºノ)";
+        else
+            return "(╯°□°）╯︵ ┻━┻";
+    }
+}
---
+++ src/test/java/TestFlipDaTable.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import frc.robot.FlipaDaTable;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class TestFlipDaTable {
+    @BeforeEach
+    void prepare(){
+        FlipaDaTable.reset();
+    }
+
+    @Test
+    void testInitialState() {
+        assertEquals(FlipaDaTable.flip(), "┬─┬ノ( º _ ºノ)");
+    }
+
+    @Test
+    void testFlipping() throws DecoderException{
+        String encoded = "28e295afc2b0e296a1c2b0efbc89e295afefb8b520e294bbe2943fe294bb";
+
+        FlipaDaTable.getInstance().nextState();
+        char[] flipped = Hex.encodeHex(FlipaDaTable.flip().getBytes());
+
+        assertTrue(new String(flipped).equals(encoded));
+    }
+}
